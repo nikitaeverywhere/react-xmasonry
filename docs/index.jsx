3a7ceff6de08add6e4ad202154e79726f8d2d8f0
ZitRos <zitros.lab@gmail.com>
2017-06-05 21:18:34 +0300

    Examples improvements

--- docs/index.jsx
+++ docs/index.jsx
@@ -10,14 +10,6 @@ window.init = function () {
                 <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/css/CardsDemo.css">CSS source</a> | <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/jsx/CardsDemo.jsx">JavaScript source</a>
             </div>
             <CardsDemo/>
-            <h1 style={{ textAlign: "center" }}>Articles Demo</h1>
-            <div className="littleGray centerText">
-                Use better CSS fixed ratio approach when possible (no image load hooks are triggered, content appears immediately)
-            </div>
-            <div className="littleGray centerText">
-                <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/css/ArticlesDemo.css">CSS source</a> | <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/jsx/ArticlesDemo.jsx">JavaScript source</a>
-            </div>
-            <ArticlesDemo/>
             <h1 style={{ textAlign: "center" }}>Content Changes Demo</h1>
             <div className="littleGray limited centerText">
                 When grid items change their size dynamically (like dynamically loaded CSS
@@ -29,6 +21,14 @@ window.init = function () {
                 <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/css/ContentChangesDemo.css">CSS source</a> | <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/jsx/ContentChangesDemo.jsx">JavaScript source</a>
             </div>
             <ContentChangesDemo/>
+            <h1 style={{ textAlign: "center" }}>Articles Demo</h1>
+            <div className="littleGray centerText">
+                Use better CSS fixed ratio approach when possible (no image load hooks are triggered, content appears immediately)
+            </div>
+            <div className="littleGray centerText">
+                <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/css/ArticlesDemo.css">CSS source</a> | <a target="_blank" href="https://github.com/ZitRos/react-xmasonry/blob/master/docs/jsx/ArticlesDemo.jsx">JavaScript source</a>
+            </div>
+            <ArticlesDemo/>
         </div>,
         document.getElementById(`demoApp`)
     );
